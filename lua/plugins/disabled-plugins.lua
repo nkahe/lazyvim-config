47b304b0bd3097c6a2800ae36f59ecbc2f488810
Henri K <12832691+nkahe@users.noreply.github.com>
2025-09-14 19:40:52 +0300

    WhichKey labels to show modifier key names
    So AltGr mapping don't show just obscure key characters

--- lua/plugins/disabled-plugins.lua
+++ lua/plugins/disabled-plugins.lua
@@ -1,6 +1,7 @@
-
--- Add all default disabled plugins here.
+-- Disabled default LazyVim plugins.
 
 return {
-  { "rafamadriz/friendly-snippets", enabled = false }
+  { "catppuccin/nvim", enabled = false },
+  { "folke/tokyonight.nvim", enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
 }
--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,20 +1,52 @@
 
 -- if true then return {} end
 
+-- return {
+--   "folke/which-key.nvim",
+--   opts = {
+--     spec = {
+--       {
+--         { "<Leader>N", group = "Notes (Obsidian)" },
+--         { "<Leader>t", group = "Terminal" },
+--         { "gr", group = "LSP" },
+--         { "ö", group = "Local leader" },
+--        -- 👇 new entry: override <Leader>? to show global WhichKey
+--         { "<Leader>?", "<cmd>WhichKey<cr>", desc = "WhichKey (global)" },
+--       }
+--     }
+--   }
+-- }
+
 return {
   "folke/which-key.nvim",
-  opts = {
-    spec = {
+  opts = function(_, opts)
+    -- extend your existing spec
+    opts.spec = vim.list_extend(opts.spec or {}, {
       {
         { "<Leader>N", group = "Notes (Obsidian)" },
         { "<Leader>t", group = "Terminal" },
         { "gr", group = "LSP" },
         { "ö", group = "Local leader" },
-       -- 👇 new entry: override <Leader>? to show global WhichKey
+        -- 👇 global WhichKey
         { "<Leader>?", "<cmd>WhichKey<cr>", desc = "WhichKey (global)" },
-      }
-    }
-  }
+
+        -- eAltGr mappings
+        { "ð", '"_d', desc = "Delete without yanking" },
+        { "Ð", '"_D', desc = "Delete line without yanking" },
+        -- { "š", function() require("flash").jump() end, desc = "Flash Search" },
+      },
+    })
+
+    -- extend key replacements
+    opts.replace = opts.replace or {}
+    opts.replace.key = vim.list_extend(opts.replace.key or {}, {
+      { "ð", "AltGr-d" },
+      { "Ð", "AltGr-D" },
+      -- { "š", "AltGr-s" },
+    })
+
+    return opts
+  end,
 }
 
 -- in normal file could be done like this
