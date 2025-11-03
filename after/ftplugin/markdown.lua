5b7a840bd79b73de08f0020ca7a7399527297d7d
Henri K <12832691+nkahe@users.noreply.github.com>
2025-11-03 15:30:00 +0200

    Markdown ftplugin to symlink.

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,11 +1,1 @@
-
--- NOTE: spell check is defined in autocmds.lua to work with LazyVim.
-
--- vim.opt.textwidth = 90     -- Set the soft wrap column
-vim.opt.colorcolumn = "90"
-vim.cmd('Wrapwidth 90')
-
-vim.cmd('setlocal nospell')
-
--- Disable diagnostics by default.
-vim.diagnostic.enable(false)
+/home/henri/.config/nvim-custom/after/ftplugin/markdown.lua
