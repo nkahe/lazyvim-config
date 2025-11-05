e13edc54ab3bab65801ff8ccd9b5902683dafe13
Henri K <12832691+nkahe@users.noreply.github.com>
2025-11-05 10:38:26 +0200

    Make vscode-colorscheme config work in other configs.

--- lua/plugins/vscode-colorscheme.lua
+++ lua/plugins/vscode-colorscheme.lua
@@ -1,30 +1,22 @@
 
-
--- if true then return {} end
-
 -- Mofiqul/vscode.nvim: Neovim/Vim color scheme inspired by Dark+ and Light+
 -- theme in Visual Studio Code - https://github.com/Mofiqul/vscode.nvim
 
 return {
-    "Mofiqul/vscode.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme vscode]])
-    end,
-    opts = {
-      -- NOTE: mini.hipatterns doesn't hilight these but still works.
+  "Mofiqul/vscode.nvim",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  opts = {
       color_overrides = {
 
         -- Normal & signcolumn bg
-        vscBack = '#1b1b1b',
+        -- vscBack = '#1b1b1b',
 
         -- Cursorline, vertical ColorColumn
-        vscCursorDarkDark = "#1f1f1f",
+        -- vscCursorDarkDark = "#1f1f1f",
 
         -- Floating windowws, terminals etc.
-        vscPopupBack = '#262626',
+        -- vscPopupBack = '#262626',
       },
 
       group_overrides = {
@@ -35,22 +27,35 @@ return {
 
         -- By default Snacks Explorer tree and directory names are too dark.
         -- Make them same as other background.
-        SnacksPickerTree = {
-          bg = '#252525',
-        },
-        SnacksPickerDirectory = {
-          bg ='#252525'
-        },
-        SnacksPickerCol = {
-          bg ='#252525'
-        },
+        SnacksPickerTree = { bg = '#252525', },
+        SnacksPickerDirectory = { bg ='#252525' },
+        SnacksPickerCol = { bg ='#252525' },
 
-      -- Define a highlight group for terminal background. These are used
-        -- in autocmds.lua
-      TermBackground = { bg = "#010b0d" },
-      TermCursorLine = { bg = "#010b0d" },
+        -- Make input box less striking.
+        SnacksPickerInputBorder = { fg = "#595959", bg = "NONE" },
+        SnacksPickerInputTitle = { fg = "#919191", bg = "NONE" },
+        SnacksPickerGitStatusUntracked = { fg = "#8b8b8b", bg = "NONE" },
+
+        NoiceCmdlinePopupBorder = { fg="#a7a7a7" },
+        NoiceCmdlinePrompt = { fg="#a7a7a7" },
+
+        -- New highlight groups used in autocmd to change terminal colors.
+        TermBackground = { bg = "#121212" },
+        TermCursorLine = { bg = "none" }
       }
-    }
+
+    },
+  config = function(_, opts)
+    -- Documentation suggest calling it. Usings only opts gave difficulties
+    -- on appling theme.
+    require('vscode').setup(opts)
+    -- In LazyVim this can be added to it's spec opts in lazy.lua.
+    vim.cmd.colorscheme "vscode"
+  end
+    -- })
+    -- opts = {
+      -- NOTE: mini.hipatterns doesn't hilight these but still works.
+
 }
 
     -- opts = function()
@@ -63,5 +68,4 @@ return {
       --   group_overrides = {
       --   },
       -- })
-
 
