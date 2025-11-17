4b4d5cd0ca31517e9ec17921e73c21d5528a20af
Henri K <12832691+nkahe@users.noreply.github.com>
2025-11-17 09:11:05 +0200

    undelete H and L

--- lua/config/keymaps-lazyvim.lua
+++ lua/config/keymaps-lazyvim.lua
@@ -1,11 +1,17 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+
+-- Get Neovim's config home directory
+-- local config_dir = vim.fn.stdpath("config") .. "/lua/config/"
 
 -- Lazyvim specific keymaps
 
 local map = vim.keymap.set
 
 -- Restore some default mappings
-pcall(vim.keymap.del, "n", "H")
-pcall(vim.keymap.del, "n", "L")
+-- pcall(vim.keymap.del, "n", "H")
+-- pcall(vim.keymap.del, "n", "L")
 pcall(vim.keymap.del, "n", "s")
 pcall(vim.keymap.del, "n", "S")
 
