10726ecda91d48f268bb7e4eab8cb4a78fcaa713
Henri K <12832691+nkahe@users.noreply.github.com>
2025-12-02 15:30:04 +0200

    Hot-reload autocmd to symlinked file.

--- lua/config/autocmds-lazyvim.lua
+++ lua/config/autocmds-lazyvim.lua
@@ -3,31 +3,29 @@
 
 pcall(vim.api.nvim_del_augroup_by_name, "lazyvim_wrap_spell")
 
--- Autosource --------------------------------------------
-
--- Watches for saves specifically in the `lua/config/keymaps.lua` and `lua/config/options.lua` files
-local config_path = vim.fn.stdpath("config") .. "/lua/config/"
-
-vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = { config_path .. "keymaps.lua", config_path .. "options.lua" },
-  callback = function(args)
-    vim.cmd("source " .. args.file)
-    print("Sourced " .. args.file)
-  end,
-})
-
-local function get_terminal_name()
-  if vim.g.neovide then
-    terminalName = "Neovide"
-  end
-  return terminalName
+-- Helper to set autogroup with prefix.
+local function augroup(name)
+  return vim.api.nvim_create_augroup("Custom_" .. name, { clear = true })
 end
 
 -- Source additional config files.
 vim.api.nvim_create_autocmd("User", {
   pattern = "VeryLazy",
+  group = augroup("source_custom_configs"),
   callback = function()
     require("config.keymaps-lazyvim")
     require("config.user-commands")
+
+    if vim.g.vscode then
+      require("config.vscode")
+    else
+      -- Start server so can open files in terminal with Neovim without having to open
+      -- them in different process.
+      if not vim.v.servername or vim.v.servername == '' then
+        vim.fn.serverstart(string.format("/tmp/nvim.%d", vim.fn.getpid()))
+      end
+
+    end
+
   end,
 })
